e9fb50fd46f169cb8836040b51bf7d434ceb394b
actingyeti <amonmanekul@gmail.com>
2017-07-14 22:37:33 -0400

    Update keymap.c

--- keyboards/mitosis/keymaps/default/keymap.c
+++ keyboards/mitosis/keymaps/default/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN },
   {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH },
   {XXXXXXX, M(VOLU), M(ESCM), KC_LALT, KC_LCTL,        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX },
-  {XXXXXXX, M(VOLD), KC_DELETE, KC_BSPC, FNKEY,        KC_LSFT, KC_SPC,  KC_ENT,  KC_TAB,  XXXXXXX }
+  {XXXXXXX, M(VOLD), SHIFT,   FNKEY, KC_BSCP,          KC_SPC,  KC_LSFT, KC_ENT,  KC_TAB,  XXXXXXX }
 },
 
 
@@ -63,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_EXLM, KC_AT, KC_HASH,  KC_DLR, KC_PERC,         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN },
   {KC_1,    KC_2,  KC_3,     KC_4,   KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0    },
   {_______, _______, KC_PSCR, KC_GRV,  KC_QUOT,       KC_MINS, KC_EQL,  KC_DOT,  KC_LBRC, KC_RBRC },
-  {XXXXXXX, _______, _______,  _______,  _______,     _______, KC_EQL,  KC_HOME, KC_PGUP, XXXXXXX },
-  {XXXXXXX, _______, _______,  _______,  _______,     _______, KC_0,    KC_END,  KC_PGDN, XXXXXXX }
+  {XXXXXXX, _______, _______,  _______,  _______,    _______, _______,  KC_HOME, KC_PGUP, XXXXXXX },
+  {XXXXXXX, _______, _______,  _______,  KC_DELETE,  _______, KC_CAPS,  KC_END,  KC_PGDN, XXXXXXX }
 },
 
 
