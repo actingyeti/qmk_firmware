00e469ed6253ab4a0b5d8995d2d1a85f6350526d
actingyeti <amonmanekul@gmail.com>
2017-07-22 17:43:43 -0400

    Update keymap.c

--- keyboards/mitosis/keymaps/default/keymap.c
+++ keyboards/mitosis/keymaps/default/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    },
   {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN },
   {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH },
-  {XXXXXXX, M(VOLU), M(ESCM), KC_LALT, KC_LCTL,        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX },
-  {XXXXXXX, M(VOLD), SHIFT,   KC_LSFT, KC_BSPC,        KC_SPC,  FNKEY,  KC_ENT,   KC_TAB,  XXXXXXX }
+  {XXXXXXX, M(VOLU), KC_LALT, SHIFT, M(ESCM),          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX },
+  {XXXXXXX, M(VOLD), KC_LCTL, KC_BSPC, KC_LSFT,        KC_SPC,  FNKEY,  KC_ENT,   KC_TAB,  XXXXXXX }
 },
 
 
@@ -63,18 +63,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_EXLM, KC_AT, KC_HASH,  KC_DLR, KC_PERC,         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN },
   {KC_1,    KC_2,  KC_3,     KC_4,   KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0    },
   {_______, _______, KC_PSCR, KC_GRV,  KC_QUOT,       KC_MINS, KC_EQL,  KC_DOT,  KC_LBRC, KC_RBRC },
-  {XXXXXXX, _______, _______,  _______,  _______,    _______, _______,  KC_HOME, KC_PGUP, XXXXXXX },
-  {XXXXXXX, _______, _______,  _______,  KC_DELETE,  _______, _______,  KC_END,  KC_PGDN, XXXXXXX }
+  {XXXXXXX, KC_HOME, KC_PGUP,  _______,  _______,    _______, _______,  _______, _______, XXXXXXX },
+  {XXXXXXX, KC_END,  KC_PGDN,  _______,  _______,    _______, _______,KC_DELETE, _______, XXXXXXX }
 },
 
 
 [_FUNCSHIFT] = { /* Function Shifted Layer, secondary alternative layer with closing brackets,
                                             and F-keys under their numpad equivalents*/
-  {_______, _______, _______, _______, _______,       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10  },
-  {_______, _______, _______, _______, _______,       _______, KC_F4,   KC_F5,   KC_F6,   KC_F11  },
-  {_______, _______, _______, _______, _______,       _______, KC_F1,   KC_F2,   KC_F3,   KC_F12  },
-  {XXXXXXX, _______, _______, _______, _______,       _______, _______, _______, _______, XXXXXXX },
-  {XXXXXXX, _______, _______, _______, _______,       _______, _______, _______, _______, XXXXXXX }
+  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,          _______, KC_F7,   KC_F8,   KC_F9,   KC_F10  },
+  {KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,          _______, KC_F4,   KC_F5,   KC_F6,   KC_F11  },
+  {KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,          _______, KC_F1,   KC_F2,   KC_F3,   KC_F12  },
+  {XXXXXXX, KC_1,    KC_2,    KC_3,    KC_4,          _______, _______, _______, _______, XXXXXXX },
+  {XXXXXXX, KC_B,    KC_T,    KC_G,    KC_SPC,        _______, _______, _______, _______, XXXXXXX }
 }
 
 };
