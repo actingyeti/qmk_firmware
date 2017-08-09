6d0e73764a76ac908ee186cd1a7c24f357a70c8e
actingyeti <amonmanekul@gmail.com>
2017-08-08 21:16:30 -0400

    Update keymap.c

--- keyboards/mitosis/keymaps/default/keymap.c
+++ keyboards/mitosis/keymaps/default/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    },
   {KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN },
   {KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH },
-  {XXXXXXX, M(VOLU), KC_LALT, SHIFT, M(ESCM),          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX },
-  {XXXXXXX, M(VOLD), KC_LCTL, KC_BSPC, KC_LSFT,        KC_SPC,  FNKEY,  KC_ENT,   KC_TAB,  XXXXXXX }
+  {XXXXXXX, M(VOLU), KC_LALT, KC_LSFT, M(ESCM),        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX },
+  {XXXXXXX, M(VOLD), KC_LCTL, SHIFT, KC_BSPC,          KC_SPC,  FNKEY,  KC_ENT,   KC_TAB,  XXXXXXX }
 },
 
 
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    cursor keys on left hand, and all symbols*/
   {KC_EXLM, KC_AT, KC_HASH,  KC_DLR, KC_PERC,         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN },
   {KC_1,    KC_2,  KC_3,     KC_4,   KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0    },
-  {_______, _______, KC_PSCR, KC_GRV,  KC_QUOT,       KC_MINS, KC_EQL,  KC_DOT,  KC_LBRC, KC_RBRC },
+  {_______, _______, KC_PSCR, KC_LBRC, KC_RBRC,       KC_MINS, KC_EQL,  KC_DOT,  KC_GRV,  KC_QUOT },
   {XXXXXXX, KC_HOME, KC_PGUP,  _______,  _______,    _______, _______,  _______, _______, XXXXXXX },
   {XXXXXXX, KC_END,  KC_PGDN,  _______,  _______,    _______, _______,KC_DELETE, _______, XXXXXXX }
 },
